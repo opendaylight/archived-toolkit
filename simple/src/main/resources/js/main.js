c6fd8e153203373655af54cd5bbc34f3d12cf9d9
Andrew Kim <h.andrew.kim@gmail.com>
2014-05-01 18:11:55 -0500

    Sample app and node server
    Simple app is included for now as a standalone
    Node development environment is included under node with symlinks to the simple app
    
    Change-Id: Ia3879862e39fb264bbc35d73ffef60a284e4a4c5
    Signed-off-by: Andrew Kim <h.andrew.kim@gmail.com>

---
+++ simple/src/main/resources/js/main.js
@@ -0,0 +1,20 @@
+// Filename: main.js
+
+require.config({
+  paths: {
+    'jquery': '/js/ext/jquery/dist/jquery',
+    'jquery-ui': '/js/ext/jquery-ui/ui/minified/jquery-ui.min',
+    'underscore': '/js/ext/underscore/underscore'
+  },
+  shim: {
+    'jquery-ui' : {
+      exports: '$',
+      deps: ['jquery']
+    }
+  }
+});
+
+require([
+  'app'
+], function(App) {
+});
